31bc14ace4be5610527edaa6897c99433a016fc5
BunniBooXx <140341233+BunniBooXx@users.noreply.github.com>
2023-10-28 02:39:40 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Navbar from './Navbar'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+
+export default class App extends Component {
+
+  constructor () {
+    super(); 
+    this.state = {
+      addItem: 0,
+      toDo: null
+    }
+  }
+
+  addItems = () => {
+    this.setState({
+      addItem: this.state.addItem += 1
+    })
+  }
+
+  item = (e) => {
+    e.preventDefault();
+    const item = e.target.item.value;
+    this.setState({
+       toDo : item
+    })
+  }
+  componentDidMount=() => {
+
+  }
+
+    
+  
+  render() {
+    return (
+      <div>
+        <Navbar anything = {this.state.toDo}/>
+
+        <form> 
+          <input placeholder="what to do next" name = "item"></input>
+          <button onSubmit={this.item}>Submit</button>
+
+          <h1> To-Do : {this.state.toDo}</h1>
+          <h1>Add Item: {this.state.addItem}</h1><button onClick={this.addItems}>+</button>
+        </form>
+      </div>
+    )
+  }
 }
-
-export default App;
